b82095525059a753bdf7d244ce7e5e6baf03aa82
francoMG <francomolocheg99@gmail.com>
2020-11-15 18:19:07 -0500

    user setting styling

--- src/app/user-settings/user-settings.component.ts
+++ src/app/user-settings/user-settings.component.ts
@@ -7,88 +7,86 @@ import { SubscriptionsService } from '../services/subscriptions.service';
 @Component({
   selector: 'app-user-settings',
   templateUrl: './user-settings.component.html',
-  styleUrls: ['./user-settings.component.css']
+  styleUrls: ['./user-settings.component.css'],
 })
 export class UserSettingsComponent implements OnInit {
-
   public notificationTypes = [];
   preferences = [];
   public options = [];
   public oldOptions = [];
-  constructor(private router:Router,private subscriptionService:SubscriptionsService,private appComponent:AppComponent, private notificationTypeService:NotificationTypesService) { }
+  constructor(
+    private router: Router,
+    private subscriptionService: SubscriptionsService,
+    private appComponent: AppComponent,
+    private notificationTypeService: NotificationTypesService
+  ) {}
   loaded = false;
   ngOnInit(): void {
+    console.log(this.appComponent.webSocketAPI.uid);
+    this.subscriptionService
+      .getByUser(this.appComponent.webSocketAPI.uid)
+      .subscribe((data) => {
+        this.preferences = data;
 
-  console.log(this.appComponent.webSocketAPI.uid)
-    this.subscriptionService.getByUser(this.appComponent.webSocketAPI.uid).subscribe(data=>{
-      this.preferences = data;
-      
-      this.getNotifTypes();
-      
-      
-    });
-    
-    
-    
+        this.getNotifTypes();
+      });
   }
 
-  getNotifTypes(){
-    this.notificationTypeService.getNotificationTypes().subscribe(data=>{
+  getNotifTypes() {
+    this.notificationTypeService.getNotificationTypes().subscribe((data) => {
       this.notificationTypes = data;
-      
-      
 
-
-      this.notificationTypes.forEach(type => {
+      this.notificationTypes.forEach((type) => {
         let chosen = false;
-        this.preferences.forEach(element2 => {
-            if(type.id === element2.notificationType_id){
-              chosen = true;
-            }
+        this.preferences.forEach((element2) => {
+          if (type.id === element2.notificationType_id) {
+            chosen = true;
+          }
         });
 
-        this.options.push({notifType:type,Chosen:chosen})
-
-        
+        this.options.push({ notifType: type, Chosen: chosen });
       });
       for (let i = 0; i < this.options.length; i++) {
-        let t = this.options[i]
-        this.oldOptions.push( {...t})
+        let t = this.options[i];
+        this.oldOptions.push({ ...t });
       }
-     this.loaded = true;
-    })
+      this.loaded = true;
+    });
   }
-  updatePreferences(){
-    console.log("new",this.options)
-    console.log("old ",this.oldOptions)
+  updatePreferences() {
+    console.log('new', this.options);
+    console.log('old ', this.oldOptions);
 
     for (let i = 0; i < this.options.length; i++) {
-      if(this.options[i].Chosen != this.oldOptions[i].Chosen){
-
-        if(this.options[i].Chosen === false){
-          console.log({user_id:this.appComponent.webSocketAPI.uid,
-            notificationType_id:this.options[i].notifType.id});
-          this.subscriptionService.deletePreference({user_id:this.appComponent.webSocketAPI.uid,
-            notificationType_id:this.options[i].notifType.id}).
-          subscribe(data=>{
-            console.log(data);
-
-          })
-        }else{
-          this.subscriptionService.postPreference({user_id:this.appComponent.webSocketAPI.uid,
-            notificationType_id:this.options[i].notifType.id}).
-            subscribe(data=>{
-              console.log(data);
+      if (this.options[i].Chosen != this.oldOptions[i].Chosen) {
+        if (this.options[i].Chosen === false) {
+          console.log({
+            user_id: this.appComponent.webSocketAPI.uid,
+            notificationType_id: this.options[i].notifType.id,
+          });
+          this.subscriptionService
+            .deletePreference({
+              user_id: this.appComponent.webSocketAPI.uid,
+              notificationType_id: this.options[i].notifType.id,
             })
+            .subscribe((data) => {
+              console.log(data);
+            });
+        } else {
+          this.subscriptionService
+            .postPreference({
+              user_id: this.appComponent.webSocketAPI.uid,
+              notificationType_id: this.options[i].notifType.id,
+            })
+            .subscribe((data) => {
+              console.log(data);
+            });
         }
-
       }
-      
     }
-    
   }
 
-  deleteAll(){
+  deleteAll() {
     this.appComponent.deleteAll();
   }
 }
